efdb903c22d8d6e7e7b8178f5949d70c8972b027
Vipul Sharma <sharmavipul9756@gmail.com>
2021-10-21 01:06:13 +0530

    Lottie animations in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Lottie from 'react-lottie'
+import animationBuilding from './Lotties/animationBuilding.json'
+const App = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationBuilding,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      return (
+        <div>
+          <Lottie 
+            options={defaultOptions}
+            height={400}
+            width={400}
+          />
+        </div>
+      );
+    }
+    
 
-export default App;
+
+export default App
